f22c7ca3f0b26639a820c441e3d7bee86d1fb282
k8abadzheva <make.me.loco@gmail.com>
2024-12-15 02:54:14 +0300

    feat: Use ViewModel in ResultFragmentAdded a ResultViewModel and ResultViewModelFactory to manage the result data. Updated ResultFragment to use the ViewModel to display the game result.

--- app/src/main/java/com/abadzheva/guessinggame/ResultFragment.kt
+++ app/src/main/java/com/abadzheva/guessinggame/ResultFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.abadzheva.guessinggame.databinding.FragmentResultBinding
 
@@ -12,6 +13,8 @@ class ResultFragment : Fragment() {
     @Suppress("ktlint:standard:backing-property-naming")
     private var _binding: FragmentResultBinding? = null
     private val binding get() = _binding!!
+    lateinit var viewModel: ResultViewModel
+    lateinit var viewModelFactory: ResultViewModelFactory
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,7 +24,11 @@ class ResultFragment : Fragment() {
         _binding = FragmentResultBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        binding.wonLost.text = ResultFragmentArgs.fromBundle(requireArguments()).result
+        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
+        viewModelFactory = ResultViewModelFactory(result)
+        viewModel = ViewModelProvider(this, viewModelFactory)[ResultViewModel::class.java]
+
+        binding.wonLost.text = viewModel.result
 
         binding.newGameButton.setOnClickListener {
             view
