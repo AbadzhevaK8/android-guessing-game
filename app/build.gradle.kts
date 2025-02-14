24588363f9227a4ad19b66338ba47397d9414f38
k8abadzheva <make.me.loco@gmail.com>
2025-02-15 02:07:35 +0300

    feat: Migrate to Compose and update dependencies
    Migrated the project to use Jetpack Compose.
    - Enabled Compose in the `buildFeatures` and set the `kotlinCompilerExtensionVersion` to "1.5.1" in app/build.gradle.kts.
    - Added Compose dependencies for material3, foundation, UI, activity, and lifecycle in `app/build.gradle.kts`.
    - Added Compose tooling dependencies for preview and UI testing.
    - Introduced `androidx.compose:compose-bom:2025.01.01` for managing Compose versions.
    - Added Material Icons dependencies.
    - Added `androidx.compose.material3.adaptive` dependency.
    - Added new library dependencies to `gradle/libs.versions.toml`, including Compose-related libraries and updated versions for `activityCompose` and `lifecycleViewmodelCompose`.
    - Added the compose compiler plugin.
    - Changed kotlin kapt to kotlin plugin in app/build.gradle.kts.
    - Updated plugin.serialization to org.jetbrains.kotlin.plugin.serialization in app/build.gradle.kts.
    - Updated versions of other dependencies.
    - Added compose compiler plugin to build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    kotlin("kapt")
+    alias(libs.plugins.compose.compiler)
+    id("org.jetbrains.kotlin.kapt")
     id("androidx.navigation.safeargs")
-    kotlin("plugin.serialization") version "2.0.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
 }
 
 android {
@@ -38,6 +39,10 @@ android {
     }
     buildFeatures {
         dataBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
 }
 
@@ -71,4 +76,45 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
 
     implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    val composeBom = platform("androidx.compose:compose-bom:2025.01.01")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    // Choose one of the following:
+    // Material Design 3
+    implementation(libs.androidx.material3)
+    // or Material Design 2
+    implementation(libs.androidx.material)
+    // or skip Material Design and build directly on top of foundational components
+    implementation(libs.androidx.foundation)
+    // or only import the main APIs for the underlying toolkit systems,
+    // such as input and measurement/layout
+    implementation(libs.androidx.ui)
+
+    // Android Studio Preview support
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
+
+    // UI Tests
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Optional - Included automatically by material, only add when you need
+    // the icons but not the material library (e.g. when using Material3 or a
+    // custom design system based on Foundation)
+    implementation(libs.androidx.material.icons.core)
+    // Optional - Add full set of material icons
+    implementation(libs.androidx.material.icons.extended)
+    // Optional - Add window size utils
+    implementation(libs.androidx.adaptive)
+
+    // Optional - Integration with activities
+    implementation(libs.androidx.activity.compose)
+    // Optional - Integration with ViewModels
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    // Optional - Integration with LiveData
+    implementation(libs.androidx.runtime.livedata)
+    // Optional - Integration with RxJava
+    implementation(libs.androidx.runtime.rxjava2)
 }
