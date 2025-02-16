72cd6c087abe76af0a26943ab5884e4dcc5c6637
k8abadzheva <make.me.loco@gmail.com>
2025-02-17 02:09:08 +0300

    Revert "feat: Integrate Compose UI into GameFragment"
    This reverts commit 0e6d28de08e048c6eb0a4d41c0c54263bbdbf1e9.

--- app/src/main/java/com/abadzheva/guessinggame/GameFragment.kt
+++ app/src/main/java/com/abadzheva/guessinggame/GameFragment.kt
@@ -4,25 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.findNavController
 import com.abadzheva.guessinggame.databinding.FragmentGameBinding
 
@@ -37,16 +21,7 @@ class GameFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding =
-            FragmentGameBinding.inflate(inflater, container, false).apply {
-                composeView.setContent {
-                    MaterialTheme {
-                        Surface {
-                            GameFragmentContent(viewModel)
-                        }
-                    }
-                }
-            }
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
         val view = binding.root
         viewModel = ViewModelProvider(this)[GameViewModel::class.java]
 
@@ -80,64 +55,5 @@ class GameFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-}
-
-@Suppress("ktlint:standard:function-naming")
-@Composable
-fun FinishGameButton(clicked: () -> Unit) {
-    Button(onClick = clicked) {
-        Text("Finish Game")
-    }
-}
-
-@Suppress("ktlint:standard:function-naming")
-@Composable
-fun EnterGuess(
-    guess: String,
-    changed: (String) -> Unit,
-) {
-    TextField(
-        value = guess,
-        label = { Text("Guess a letter") },
-        onValueChange = changed,
-    )
-}
-
-@Suppress("ktlint:standard:function-naming")
-@Composable
-fun GuessButton(clicked: () -> Unit) {
-    Button(onClick = clicked) {
-        Text("Guess!")
-    }
-}
-
-@Suppress("ktlint:standard:function-naming")
-@Composable
-fun GameFragmentContent(viewModel: GameViewModel) {
-    val guess = remember { mutableStateOf("") }
-
-    Column(
-        modifier =
-            Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-    ) {
-        EnterGuess(guess.value) { guess.value = it }
-
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-        ) {
-            GuessButton {
-                viewModel.makeGuess(guess.value.uppercase())
-                guess.value = ""
-            }
-            FinishGameButton {
-                viewModel.finishGame()
-            }
-        }
     }
 }
